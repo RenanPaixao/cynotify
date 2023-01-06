253924d24753e8bd3f8a3e07863309b276407cf4
renan <renanlemos21@gmail.com>
2023-01-05 21:31:35 -0300

    feat: add export with a conditional play

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 /// <reference types="cypress" />
 
-after(()=>{
-	cy.readFile('src/assets/notification.mp3', 'base64').then((sound)=>{
-		const audio = new Audio(`data:audio/mp3;base64,${sound}`)
-		audio.play()
+function play(){
+	after(() => {
+		cy.readFile('src/assets/notification.mp3', 'base64').then((sound) => {
+			const audio = new Audio(`data:audio/mp3;base64,${sound}`)
+			audio.play()
+		})
 	})
-})
+}
+
+play()
+
+export default function(shouldPlay){
+	if(shouldPlay){
+		play()
+	}
+}
