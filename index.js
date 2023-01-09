652610bae5caa4ac21e114197ebe3b2dab748e36
renan <renanlemos21@gmail.com>
2023-01-09 00:49:52 -0300

    fix error in package json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 Cypress.Commands.add('notify', () => {
-	cy.readFile('src/assets/notification.mp3', 'base64').then((sound) => {
+	cy.readFile('./src/assets/notification.mp3', 'base64').then((sound) => {
 		const audio = new Audio(`data:audio/mp3;base64,${sound}`)
 		audio.play()
 	})
