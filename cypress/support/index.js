12b71b9f3401ce785acbee893cfe7a73d55e59b3
renan <renanlemos21@gmail.com>
2023-01-09 00:27:29 -0300

    fix: trying to fix miss dependency error

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,2 +1,2 @@
 import './commands'
-import '../../src'
+import 'cynotify'
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/**
- * Play a notification sound.
- */
-Cypress.Commands.add('playSound', () => {
+Cypress.Commands.add('notify', () => {
 	cy.readFile('src/assets/notification.mp3', 'base64').then((sound) => {
 		const audio = new Audio(`data:audio/mp3;base64,${sound}`)
 		audio.play()
