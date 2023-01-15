8908647f51bce3915e27094eba52bd308dee4bc7
renan <renanlemos21@gmail.com>
2023-01-15 01:37:52 -0300

    feat: remove command log

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 Cypress.Commands.add('notify', () => {
-	cy.readFile('node_modules/cynotify/src/assets/notification.mp3', 'base64').then((sound) => {
+	cy.readFile('node_modules/cynotify/src/assets/notification.mp3', 'base64', { log: false }).then((sound) => {
 		const audio = new Audio(`data:audio/mp3;base64,${sound}`)
 		audio.play()
 	})
